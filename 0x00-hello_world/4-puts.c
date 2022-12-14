a5f45c57c84d490eec625891515f928f6cb607ae
Clifford Emeka <emclidon@gmail.com>
2022-12-14 06:09:42 -0800

    4-puts c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints out a string
+ * Return: return 0
+ */
+int main(void)
+{
+	char str_to_print[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str_to_print);
+
+	return (0);
+}
